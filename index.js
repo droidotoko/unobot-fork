3245e75cd6de7799f02d885d87c6bfce90abe3e2
droidotoko <droidophilic@gmail.com>
2023-11-19 01:42:35 +0530

    add a nodejs server to establish http

--- index.js
+++ index.js
@@ -73,19 +73,19 @@ if (isMaster) {
 		await updateUserRanks();
 	}, 43200000);
 
-	// const port = process.env.PORT || 4000;
+	const port = process.env.PORT || 4000;
 
-	// const requestHandler = (request, response) => {
-	// 	response.end("Hello Node.js Server!");
-	// };
+	const requestHandler = (request, response) => {
+		response.end("Hello Node.js Server!");
+	};
 
-	// const server = http.createServer(requestHandler);
+	const server = http.createServer(requestHandler);
 
-	// server.listen(port, (err) => {
-	// 	if (err) {
-	// 		return console.log("something bad happened", err);
-	// 	}
+	server.listen(port, (err) => {
+		if (err) {
+			return console.log("something bad happened", err);
+		}
 
-	// 	console.log(`server is listening on ${port}`);
-	// });
+		console.log(`server is listening on ${port}`);
+	});
 }
